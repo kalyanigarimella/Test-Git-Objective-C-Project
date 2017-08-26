5ce7843391b26145873d926369036d62d01fad19
kalyanisas <kalyani.sastry@olivo.in>
2017-08-26 23:16:29 +0530

    added a test class

---
+++ Git Test/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  Git Test
+//
+//  Created by Kalyani on 26/08/17.
+//  Copyright © 2017 kalyani. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
